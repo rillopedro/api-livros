6d26594e9b3f3de9ce3bc85275b549855643daba
rillopedro <pedrorillo08@gmail.com>
2026-09-17 09:12:08 -0300

    Tudo pronto da parte 3

--- database/_biblioteca_db_.sql
+++ database/_biblioteca_db_.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Set-2026 às 12:51
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
+-- Tempo de geração: 17/09/2026 às 14:09
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,25 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `biblioteca_db`
 --
-CREATE DATABASE IF NOT EXISTS `biblioteca_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `biblioteca_db`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `livros`
+-- Estrutura para tabela `livros`
 --
 
 CREATE TABLE `livros` (
   `id` int(11) NOT NULL,
-  `titulo` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `autor` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `autor` varchar(120) NOT NULL,
   `ano_publicacao` int(11) NOT NULL,
   `disponivel` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Extraindo dados da tabela `livros`
+-- Despejando dados para a tabela `livros`
 --
 
 INSERT INTO `livros` (`id`, `titulo`, `autor`, `ano_publicacao`, `disponivel`) VALUES
@@ -50,21 +48,21 @@ INSERT INTO `livros` (`id`, `titulo`, `autor`, `ano_publicacao`, `disponivel`) V
 --
 
 --
--- Índices para tabela `livros`
+-- Índices de tabela `livros`
 --
 ALTER TABLE `livros`
   ADD PRIMARY KEY (`id`),
   ADD KEY `ix_livros_id` (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `livros`
 --
 ALTER TABLE `livros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
